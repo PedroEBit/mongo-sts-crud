178343ada1c949b8c703ad8437f0182e3c7eb57e
Pedro Ernesto <peupedreira@hotmail.com>
2024-03-22 13:06:13 -0300

    PUT funcionando

--- src/main/java/com/pedrobit/mongocrud/services/UserService.java
+++ src/main/java/com/pedrobit/mongocrud/services/UserService.java
@@ -22,20 +22,32 @@ public class UserService {
 	}
 	
 	public User findById(String id) {
-		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public User insert(User obj) {
-		return repo.insert(obj);
+	public User insert(User user) {
+		return repo.insert(user);
 	}
 	
 	public void delete(String id) {
 		repo.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repo.save(newUser);
+		}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
+
 
 }
